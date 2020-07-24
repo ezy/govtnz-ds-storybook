916d4bc959b57abc1c934d7f8063d285c43a4a89
Ezy <ezra@passionfruit.co.nz>
2020-07-24 17:32:46 +1200

    make build more generic for folder target

--- bin/build-storybook.js
+++ bin/build-storybook.js
@@ -2,34 +2,38 @@ const fs = require("fs");
 const glob = require("glob-promise");
 const path = require("path");
 
-const writeStories = (sourceFolder, nameSpace) => {
-  const htmlFile = fs.readFileSync(
-    `src/template-sources/${sourceFolder}/${nameSpace}.html`
-  );
+const makeStorybook = (sourceFolder) => {
+  const writeStories = (nameSpace) => {
+    const htmlFile = fs.readFileSync(
+      `src/template-sources/${sourceFolder}/${nameSpace}.html`
+    );
 
-  const storyPattern = () => `import { html } from "lit-html";
-import "../template-sources/${sourceFolder}/${nameSpace}.css";
+    const storyPattern = () => `import { html } from "lit-html";
+      import "../template-sources/${sourceFolder}/${nameSpace}.css";
+      
+      export default {
+          title: "${sourceFolder}",
+          component: "${nameSpace}",
+      };
+      
+      export const ${nameSpace} = () => html\`${htmlFile}\`;`;
 
-export default {
-    title: "${sourceFolder}",
-    component: "${nameSpace}",
-};
+    const storyPath = `src/stories/${nameSpace}.stories.js`;
+    fs.writeFileSync(storyPath, storyPattern());
+  };
 
-export const ${nameSpace} = () => html\`${htmlFile}\`;`;
-
-  const storyPath = `src/stories/${nameSpace}.stories.js`;
-  fs.writeFileSync(storyPath, storyPattern());
-};
-
-glob(path.join(`src/template-sources/nzgovt-import`, "*.html"))
-  .then((files) => {
-    files.map((file) => {
-      file = file
-        .replace("src/template-sources/nzgovt-import/", "")
-        .replace(".html", "");
-      writeStories("nzgovt-import", file);
+  glob(path.join(`src/template-sources/${sourceFolder}`, "*.html"))
+    .then((files) => {
+      files.map((file) => {
+        const fileBaseName = file
+          .replace(`src/template-sources/${sourceFolder}/`, "")
+          .replace(".html", "");
+        writeStories(fileBaseName);
+      });
+    })
+    .catch((error) => {
+      throw new Error(error);
     });
-  })
-  .catch((error) => {
-    throw new Error(error);
-  });
+};
+
+makeStorybook("nzgovt-import");

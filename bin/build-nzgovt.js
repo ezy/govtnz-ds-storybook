6196d81a046fe82bad87f21f556ff0483a1ba9d6
Ezy <ezra@passionfruit.co.nz>
2020-07-24 16:19:36 +1200

    copy files to dir with script

---
+++ bin/build-nzgovt.js
@@ -0,0 +1,18 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+const htmlPath = "node_modules/@govtnz/ds/build/html";
+const cssPath = "node_modules/@govtnz/ds/build/css";
+
+const target = path.join("src", "template-sources", "nzgovt-import");
+
+try {
+  console.log(
+    "Begin copying html and css from nzgovt-ds to src/template-sources"
+  );
+  fs.copySync(htmlPath, target);
+  fs.copySync(cssPath, target);
+  console.log("Copying nzgovt-ds to src/template-sources complete");
+} catch (err) {
+  console.error(err);
+}

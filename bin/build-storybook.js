6187f9638d4cf26ce7fa21623402c039f2a99b7c
Ezy <ezra@passionfruit.co.nz>
2020-07-24 17:26:17 +1200

    build automation for nz-govt stories first pass

--- bin/build-storybook.js
+++ bin/build-storybook.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
+const glob = require("glob-promise");
+const path = require("path");
 
-const writeStories = async (sourceFolder, nameSpace) => {
-  const filePath = `../template-sources/${sourceFolder}`;
+const writeStories = (sourceFolder, nameSpace) => {
   const htmlFile = fs.readFileSync(
     `src/template-sources/${sourceFolder}/${nameSpace}.html`
   );
+
   const storyPattern = () => `import { html } from "lit-html";
-import "${filePath}/${nameSpace}.css";
+import "../template-sources/${sourceFolder}/${nameSpace}.css";
 
 export default {
     title: "${sourceFolder}",
@@ -16,12 +18,18 @@ export default {
 export const ${nameSpace} = () => html\`${htmlFile}\`;`;
 
   const storyPath = `src/stories/${nameSpace}.stories.js`;
-
   fs.writeFileSync(storyPath, storyPattern());
 };
 
-try {
-  writeStories("nzgovt-import", "Button");
-} catch (error) {
-  throw new Error(error);
-}
+glob(path.join(`src/template-sources/nzgovt-import`, "*.html"))
+  .then((files) => {
+    files.map((file) => {
+      file = file
+        .replace("src/template-sources/nzgovt-import/", "")
+        .replace(".html", "");
+      writeStories("nzgovt-import", file);
+    });
+  })
+  .catch((error) => {
+    throw new Error(error);
+  });

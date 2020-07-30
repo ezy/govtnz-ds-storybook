0ae2a6c23149ccaa59c06244584aafe703b1ad2d
Ezy <ezra@passionfruit.co.nz>
2020-07-30 13:19:03 +1200

    switch generator to make mdx files instead of js

--- bin/build-storybook.js
+++ bin/build-storybook.js
@@ -1,39 +1,63 @@
 const fs = require("fs");
+const fsPromises = fs.promises;
 const glob = require("glob-promise");
 const path = require("path");
 
-const makeStorybook = (sourceFolder) => {
-  const writeStories = (nameSpace) => {
-    const htmlFile = fs.readFileSync(
-      `src/template-sources/${sourceFolder}/${nameSpace}.html`,
-    );
+const readFile = (fileName) => {
+  return new Promise((resolve) => {
+    fs.readFile(fileName, "utf8", (err, data) => {
+      if (err) throw err;
+      return data;
+    });
+    resolve();
+  });
+};
 
-    const storyPattern = () => `import { html } from "lit-html";
+const makeStorybook = (sourceFolder) => {
+  let writeStories = async (nameSpace) => {
+    const filePath = `src/template-sources/${sourceFolder}/${nameSpace}`;
+    let htmlfile = "",
+      mdxFile = "";
+    try {
+      htmlfile = await fsPromises.readFile(`${filePath}.html`);
+      if (await fs.existsSync(`${filePath}.mdx`)) {
+        mdxFile = await fsPromises.readFile(`${filePath}.mdx`);
+      }
+      const storyPattern = () => `import { Story, Preview, Meta } from "@storybook/addon-docs/blocks";
+      import { action } from "@storybook/addon-actions";
+      
       import "../template-sources/${sourceFolder}/${nameSpace}.css";
       
-      export default {
-          title: "${sourceFolder}",
-          component: "${nameSpace}",
-      };
+      # Storybook Docs for ${nameSpace}
       
-      export const ${nameSpace} = () => html\`${htmlFile}\`;`;
-
-    const storyPath = `src/stories/${nameSpace}.stories.js`;
-    fs.writeFileSync(storyPath, storyPattern());
-  };
-
-  glob(path.join(`src/template-sources/${sourceFolder}`, "*.html"))
-    .then((files) => {
+      <Meta title="${sourceFolder}" />
+      
+      ## Story definition
+      
+      Hallelujah! HTML is working out of the box without modification.
+      How you like them apples?!
+      
+      ${mdxFile}
+      
+      <Story name="${nameSpace}" height="100px">
+        {\` ${htmlFile.toString().replace(/\n/g, "")} \`}
+      </Story>
+      `;
+      const storyPath = `src/stories/${nameSpace}.stories.mdx`;
+      fs.writeFileSync(storyPath, storyPattern());
+      let files = await glob(
+        path.join(`src/template-sources/${sourceFolder}`, "*.html"),
+      );
       files.map((file) => {
         const fileBaseName = file
           .replace(`src/template-sources/${sourceFolder}/`, "")
           .replace(".html", "");
         writeStories(fileBaseName);
       });
-    })
-    .catch((error) => {
+    } catch (error) {
       throw new Error(error);
-    });
+    }
+  };
 };
 
 makeStorybook("govtnz-import");
--- src/stories/Li.stories.js
+++
@@ -1,9 +0,0 @@
-import { html } from "lit-html";
-import "../template-sources/govtnz-import/Li.css";
-
-export default {
-  title: "govtnz-import",
-  component: "Li",
-};
-
-export const Li = () => html`<li>Example text</li> `;

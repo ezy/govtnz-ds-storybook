47ad8dd81db1d1b5ad91ac93a176aecef007b8e7
Ezy <ezra@passionfruit.co.nz>
2020-07-30 12:34:47 +1200

    start scaffolding for mdx over js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,17 @@
 module.exports = {
   logLevel: "debug",
-  addons: ["@storybook/addon-a11y", "@storybook/addon-storysource"],
+  stories: ["../src/stories/*.stories.*"],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-actions",
+    "@storybook/addon-backgrounds",
+    "@storybook/addon-controls",
+    "@storybook/addon-events",
+    "@storybook/addon-jest",
+    "@storybook/addon-knobs",
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/addon-viewport",
+  ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 /* global window */
-import { configure, addParameters } from "@storybook/web-components";
+import { addParameters } from "@storybook/web-components";
+
+const SOURCE_REGEX = /^\(\) => [`'"](.*)['`"]$/;
 
 addParameters({
   a11y: {
@@ -11,16 +13,9 @@ addParameters({
   },
   docs: {
     iframeHeight: "200px",
+    transformSource: (src) => {
+      const match = SOURCE_REGEX.exec(src);
+      return match ? match[1] : src;
+    },
   },
 });
-
-// force full reload to not reregister web components
-const req = require.context("../src/stories", true, /\.stories\.(js|mdx)$/);
-configure(req, module);
-if (module.hot) {
-  module.hot.accept(req.id, () => {
-    const currentLocationHref = window.location.href;
-    window.history.pushState(null, null, currentLocationHref);
-    window.location.reload();
-  });
-}

b5cbb1b19ceb8c1eae1fba8d7037254433dfb7c5
Ezy <ezra@passionfruit.co.nz>
2020-08-03 13:44:18 +1200

    generating with custom folder

--- bin/build-templates.js
+++ bin/build-templates.js
@@ -1,24 +1,61 @@
-const { generateTemplates } = require("metacomponent");
+const fs = require("fs");
 const jsdom = require("jsdom");
+const glob = require("glob-promise");
+const path = require("path");
 
-const templateType = {
-  REACT: 'react',
-  REACT_STYLED: 'react-styled-components',
-  VUE: 'vue',
-  VUE_JSX: 'vue-jsx',
-  ANGULAR: 'angular',
-  EMBER: 'ember'
-}
+const { generateTemplates } = require("metacomponent");
+
+const fsPromises = fs.promises;
+
+const generateFilesOfType = 'REACT';
+const templateType = (type, nameSpace) => {
+  const templates = {
+    REACT: `react/${nameSpace}.tsx`,
+    REACT_STYLED: `react-styled-components/${nameSpace}.tsx`,
+    VUE: `vue/${nameSpace}.js`,
+    VUE_JSX: `vue-jsx/${nameSpace}.ts`,
+    ANGULAR: `angular/${nameSpace}.ts`
+  };
+  return templates[type]
+};
 
 const { JSDOM } = jsdom;
 const jsdomInstance = new JSDOM(``, { pretendToBeVisual: true });
 
-const result = generateTemplates({
-  domDocument: jsdomInstance.window.document,
-  templateId: "test",
-  metaHTMLString: "<p>hello</p>",
-  cssString: "p { color: red }",
-  haltOnErrors: true,
-});
-jsdomInstance.window.close();
-console.log(result);
+const makeJSFiles = async (sourceFolder) => {
+  let writeJS = async (nameSpace) => {
+    const filePath = `src/template-sources/${sourceFolder}/${nameSpace}`;
+    let htmlFile = "",
+      cssFile = "";
+    try {
+      htmlFile = await fsPromises.readFile(`${filePath}.html`);
+      cssFile = await fsPromises.readFile(`${filePath}.css`);
+    } catch (error) {
+      throw new Error(error);
+    }
+    const result = generateTemplates({
+      domDocument: jsdomInstance.window.document,
+      templateId: nameSpace,
+      metaHTMLString: htmlFile.toString(),
+      cssString: cssFile.toString(),
+      haltOnErrors: true,
+    });
+    jsdomInstance.window.close();
+    const fileNameSpace = templateType(generateFilesOfType, nameSpace);
+    const target = `dist/${fileNameSpace}`;
+    await fsPromises.mkdir(`dist/${generateFilesOfType.toLowerCase()}`, { recursive: true });
+    fs.writeFileSync(target, result.files[fileNameSpace]);
+  };
+  let files = await glob(
+    path.join(`src/template-sources/${sourceFolder}`, "*.html"),
+  );
+  files.map((file) => {
+    const fileBaseName = file
+      .replace(`src/template-sources/${sourceFolder}/`, "")
+      .replace(".html", "");
+    writeJS(fileBaseName);
+  });
+};
+
+// makeJSFiles("govtnz-import");
+makeJSFiles("custom");
